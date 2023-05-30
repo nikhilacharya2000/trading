adc78f13c65b8f83e9bf4352e5a508fe92a450f7
rahul soni <rahulsoni95888@gmail.com>
2023-05-30 21:24:26 +0530

    show data

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -15,34 +15,36 @@ use Illuminate\Support\Facades\Http;
 class HomeController extends Controller
 {
 
-     public function index()
-     {
-         // $accessKey = 'a726603e54efab53c940dd64509e5916';
-         // $symbol = 'AAPL';
-
-         // $apiEndpoint = 'https://api.marketstack.com/v1/tickers/'.$symbol.'/eod'
-         // $queryParams = [
-         //     'access_key' => $accessKey
-         // ];
-
-         // try {
-         //     $response = Http::get($apiEndpoint, $queryParams);
-         //     $apiResult = $response->json();
-
-         //     $apiData = [];
-         //     if (isset($apiResult['data'])) {
-         //         $apiData = $apiResult['data'];
-         //     }
-         // } catch (\Exception $e) {
-         //     // Log the exception
-         //     \Log::error($e->getMessage());
-    
-         //     // Handle the exception if the API request fails
-         //     $apiData = null;
-         // }
-
-         return view('frontend.index');
-     }
+   public function index()
+   {
+       $accessKey = 'a726603e54efab53c940dd64509e5916';
+       $symbol = 'MSFT';
+   
+       $apiEndpoint = 'http://api.marketstack.com/v1/tickers/'.$symbol.'/eod';
+       $queryParams = [
+           'access_key' => $accessKey
+       ];
+   
+       try {
+           $response = Http::get($apiEndpoint, $queryParams);
+          
+           $apiResult = $response->json();
+           
+           $apiData = [];
+           if (isset($apiResult['data'])) {
+               $apiData = $apiResult['data']['eod'];
+              
+           }
+       } catch (\Exception $e) {
+           // Log the exception
+           \Log::error($e->getMessage());
+   
+           // Handle the exception if the API request fails
+           $apiData = null;
+       }
+   
+       return view('frontend.index', ['apiData' => $apiData]);
+   }
 
    // News Details
    public function viewNews(Blog $blog)
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,14 +3,44 @@
 @section('title', 'Home')
 
 @section('content')
+<!-- frontend/index.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Stock Market Data</title>
+</head>
+<body>
+    <h1>Stock Market Data for AAPL</h1>
 
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>API Data Display</title>
-    </head>
-    <body>
-    
-    </html>
+    @if ($apiData)
+        <table>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Open</th>
+                    <th>High</th>
+                    <th>Low</th>
+                    <th>Close</th>
+                    <th>Volume</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($apiData as $data)
+                    <tr>
+                        <td>{{ $data['date'] }}</td>
+                        <td>{{ $data['open'] }}</td>
+                        <td>{{ $data['high'] }}</td>
+                        <td>{{ $data['low'] }}</td>
+                        <td>{{ $data['close'] }}</td>
+                        <td>{{ $data['volume'] }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <p>Failed to retrieve stock market data. Please try again later.</p>
+    @endif
+</body>
+</html>
 
 @endsection
